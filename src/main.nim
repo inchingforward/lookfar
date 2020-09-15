8789d6a63a1ea902bf114f1e73ce52ced7a8bfdb
Mike Janger <inchingforward@gmail.com>
2020-09-15 00:02:44 -0500

    Added simple ship, idle animation.

--- src/main.nim
+++ src/main.nim
@@ -1,9 +1,17 @@
 import nico
+import nico/vec
 import dom
 
-var buttonDown = false
-var x = screenWidth div 2
-var y = screenHeight div 2
+
+type
+  Ship = ref object of RootObj
+    orig: Vec2f
+    pos: Vec2f
+    vel: Vec2f
+    bounce: float32
+    rad: int
+
+var ship: Ship
 
 proc gameInit() =
   loadFont(0, "font.png")
@@ -11,23 +19,37 @@ proc gameInit() =
   # Remove key listeners...we'll be handling game movements.
   dom.window.onkeydown = proc(event: dom.Event) =
     discard
-
   dom.window.onkeyup = proc(event: dom.Event) =
     discard
+
+  ship = new(Ship)
+  ship.orig.x = screenWidth.float32 / 2.0
+  ship.orig.y = screenHeight.float32 / 2.0
+  ship.pos.x = ship.orig.x
+  ship.pos.y = ship.orig.y
+  ship.vel.x = 5.0
+  ship.vel.y = ship.vel.x
+  ship.rad = 3
+  ship.bounce = 1.5
 
   # Testing
   var btn = getElementById("btn")
   btn.addEventListener("click") do(e: dom.Event):
-    x += 5
-    y += 5
+    ship.pos.x += 5
 
 proc gameUpdate(dt: float32) =
-  buttonDown = btn(pcA)
+  var yoffset = abs(ship.orig.y - ship.pos.y)
+
+  if (yoffset > ship.bounce):
+    ship.vel.y *= -1
+
+  ship.pos.y += ship.vel.y * dt
+
 
 proc gameDraw() =
   cls()
-  setColor(if buttonDown: 7 else: 3)
-  printc("hello world", x, y)
+  setColor(3)
+  circ(ship.pos.x, ship.pos.y, ship.rad)
 
 nico.init("inchfwd", "Lookfar")
 nico.createWindow("Lookfar", 128, 128, 4, false)
